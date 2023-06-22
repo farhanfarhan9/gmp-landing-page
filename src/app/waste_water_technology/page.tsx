e3507700189e7c49b6b44fc73aa4c01178eb8735
cecep31 <cecepjanuardi31@gmail.com>
2023-06-22 14:25:08 +0700

    product

--- src/app/waste_water_technology/page.tsx
+++ src/app/waste_water_technology/page.tsx
@@ -1,15 +1,56 @@
-import React from 'react'
-import Banner from '@/components/page/banner'
+import React from "react";
+import Banner from "@/components/page/banner";
+import Product from "@/components/page/Product";
 
 const page = () => {
-    const data = {
-        path: "Home / Waste Water Technology"
-    }
+  const product = [
+    {
+      title: "Membrane Bio Reactor (MBR)",
+      image: "/img/product/product1.png",
+      image2: "",
+      text: "Membrane Bio Reactor (MBR) adalah sistem yang menggunakan kombinasi antara membrane proses seperti ultrafiltration/microfiltration dengan proses biologi. Sistem ini sangat membantu dalam menghemat tempat dan mempersingkat start-up bioreactor.",
+      text2:
+        "<p>Keunggulan Sistem MBR vs Sistem Conventional:</p> <ol> <li>Output quality yang sangat jernih dan konsisten</li> <li>Waktu tinggal sludge yang tinggi tanpa kekhawatiran akan masalah di pengendapan (karena clarifier tidak diperlukan)</li> <li>Sludge production yang rendah</li> <li>Waktu start-up yang sangat singkat</li> <li>Area yang dibutuhkan sangat kecil (karena clarifier tidak diperlukan)</li> <li>Penggunaan kimia yang sangat rendah (karena clarifier tidak diperlukan)</li> <li>Operasional Cost yang sangat rendah (automasi yang sangat tinggi)</li> <li>Capital Cost yang rendah (karena clarifier tidak diperlukan).</li> </ol>",
+    },
+    {
+      title: "Moving Bed Biofilm Reactor (MBBR)",
+      image: "/img/product/product1.png",
+      image2: "",
+      text: "Moving bed biofilm reactor (MBBR) merupakan solusi dari permasalahan kapasitas limbah yang bertambah akan tetapi ada kendala untuk melakukan penambahan unit bangunan. MBBR ini termasuk dalam kelompok attached growth reactor, yaitu reaktor dimana mikrobiologi tumbuh di permukaan media tertentu kemudian membentuk lapisan film yang berfungsi sebagai semacam filter untuk air limbah.",
+      text2: "",
+    },
+    {
+      title: "Reverse Osmosis",
+      image: "/img/product/product1.png",
+      image2: "",
+      text: "Reverse Osmosis adalah suatu metode penyaringan yang dapat menyaring berbagai molekul besar dan ion-ion dari suatu larutan dengan cara memberi tekanan pada larutan ketika larutan itu berada di salah satu sisi membran seleksi (lapisan penyaring).",
+      text2: "",
+    },
+    {
+      title: "Dissolved Air Flotation",
+      image: "/img/product/product1.png",
+      image2: "",
+      text: "DAF/Dissolved Air Floatation adalah sistem untuk memisahkan coagulated dan flocculated solid dengan menggunakan bubble udara yang sangat kecil yang akan mengapungkan zat-zat yang akan dipisahkan dari dalam air. Design DAF yang customizable dan akurat jugalah sangat krusial dalam teknologi IPAL.",
+      text2: "",
+    },
+  ];
+  const data = {
+    path: "Home / Waste Water Technology",
+  };
   return (
     <>
-    <Banner path={data.path} title='' image='' />
+      <Banner
+        path={data.path}
+        title="Waste Water Technology"
+        image="/img/product/bannerproduct.jpg"
+      />
+      <div className="max-w-7xl mx-auto my-10">
+        {product.map((val, index) => (
+          <Product key={index} data={val} />
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
---
+++ src/components/page/Product.tsx
@@ -0,0 +1,45 @@
+"use client";
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Product = ({ data }: { data: any }) => {
+  const [expand, setexpand] = useState(false);
+  return (
+    <div className="grid grid-cols-2 py-10">
+      <div>
+        <Image
+          className="w-full"
+          src={data.image}
+          unoptimized
+          height={100}
+          width={100}
+          alt=""
+        />
+      </div>
+      <div className="flex flex-col justify-center">
+        <div className="mb-5 font-bold">{data.title}</div>
+        <div>{data.text}</div>
+        {expand && (
+          <div
+            className="prose pts-20 text-black"
+            dangerouslySetInnerHTML={{
+              __html: "<div>" + data.text2 + "</div>",
+            }}
+          />
+        )}
+        <div>
+          <button
+            onClick={() => {
+              setexpand(!expand);
+            }}
+            className="bg-green-800 text-white font-semibold px-11 py-4 mt-7"
+          >
+            {expand ? "Read less" : "Read More"}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;

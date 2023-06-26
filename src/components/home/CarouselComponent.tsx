407c1f70b244d749ec912c48b2af6eadf67514c2
nwaki1 <waki4d@gmail.com>
2023-06-26 14:30:25 +0700

    - progress carousel

--- src/components/home/CarouselComponent.tsx
+++ src/components/home/CarouselComponent.tsx
@@ -1,58 +1,57 @@
 "use client";
 import React, { useState } from "react";
 import "@/app/Carousel.css";
-import Image from "next/image";
 
-const Carousel = () => {
-  const [currentSlide] = useState(1);
-  const [images, setImages] = useState([
+function Carousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const images = [
     "/foto1.png",
     "/foto2.png",
-    "/foto3.png"
-  ]);
+    "/foto3.png",
+    // ... daftar gambar lainnya
+  ];
 
-  const handleImageClick = (index: number) => {
-    setImages(prevImages => {
-      if (index === 0) {
-        const reorderedImages = [...prevImages];
-        const clickedImage = reorderedImages.splice(2, 1)[0];
-        reorderedImages.splice(0, 0, clickedImage);
-        return reorderedImages;
-      } else if (index === 2) {
-        const reorderedImages = [...prevImages];
-        const clickedImage = reorderedImages.splice(0, 1)[0];
-        reorderedImages.splice(2, 0, clickedImage);
-        return reorderedImages;
-      } else {
-        return prevImages;
-      }
-    });
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
+  };
+
+  const previousSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? images.length - 1 : prevSlide - 1));
   };
 
   return (
-    <div>
-      <div className="flex">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            className={`flex items-center ${index === 0 ? "" : "ml-3"}`}
-            onClick={() => handleImageClick(index)}
-            style={{ cursor: "pointer" }} // Add cursor pointer style
-          >
-            <Image
-              unoptimized
-              width={100}
-              height={100}
+    <div className="carousel">
+      <button className="prev-button" onClick={previousSlide}>
+        <span className="arrow">&#8592;</span>
+      </button>
+      <div className="slider-container">
+        {images.map((image, index) => {
+          let position = index - currentSlide;
+          if (position < 0) {
+            position += images.length;
+          }
+          if (position > images.length / 2) {
+            position -= images.length;
+          }
+          const style = {
+            transform: `translateX(${position * 100}%) scale(${position === 0 ? 1.2 : 0.8})`,
+            opacity: position === 0 ? 1 : 0.5,
+          };
+          return (
+            <img
+              key={index}
+              className={`slide${position === 0 ? ' active' : ''}`}
               src={image}
               alt={`Slide ${index}`}
-              className={`${index === currentSlide ? "w-96" : "w-72"}`}
-              style={{ opacity: index === 0 || index === 2 ? 0.5 : 1 }} // Set opacity to 0.5 for index 0 and 2
             />
-          </div>
-        ))}
+          );
+        })}
       </div>
+      <button className="next-button" onClick={nextSlide}>
+        <span className="arrow">&#8594;</span>
+      </button>
     </div>
   );
-};
+}
 
 export default Carousel;

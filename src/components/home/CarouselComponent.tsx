cd63f19bc07283b8e97b7d9d15820cdd833dcfda
nwaki1 <waki4d@gmail.com>
2023-06-26 15:53:28 +0700

    - carousel progress 2

--- src/components/home/CarouselComponent.tsx
+++ src/components/home/CarouselComponent.tsx
@@ -4,12 +4,11 @@ import "@/app/Carousel.css";
 
 function Carousel() {
   const [currentSlide, setCurrentSlide] = useState(0);
-  const images = [
-    "/foto1.png",
-    "/foto2.png",
-    "/foto3.png",
-    // ... daftar gambar lainnya
-  ];
+  const [images, setImages] = useState([
+    { image: "/foto1.png", active: false },
+    { image: "/foto2.png", active: true },
+    { image: "/foto3.png", active: false },
+  ]);
 
   const nextSlide = () => {
     setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
@@ -19,37 +18,59 @@ function Carousel() {
     setCurrentSlide((prevSlide) => (prevSlide === 0 ? images.length - 1 : prevSlide - 1));
   };
 
+  const handleImageClick = (index: number) => {
+    setImages((prevImages) =>
+      prevImages.map((image, i) => {
+        return { ...image, active: false };
+      })
+    );
+
+    setImages(prevImages => {
+      if (index === 0) {
+        const reorderedImages = [...prevImages];
+        const clickedImage = reorderedImages.splice(2, 1)[0];
+        reorderedImages.splice(0, 0, clickedImage);
+        return reorderedImages;
+      } else if (index === 2) {
+        const reorderedImages = [...prevImages];
+        const clickedImage = reorderedImages.splice(0, 1)[0];
+        reorderedImages.splice(2, 0, clickedImage);
+        return reorderedImages;
+      } else {
+        return prevImages;
+      }
+    });
+
+    setTimeout(() => {
+      setImages((prevImages) =>
+        prevImages.map((image, index) => {
+          return { ...image, active: index === 1 ? true : false };
+        })
+      );
+    }, 500); // 2000 milliseconds delay (2 seconds)
+  };
+
   return (
     <div className="carousel">
-      <button className="prev-button" onClick={previousSlide}>
+      {/* <button className="prev-button" onClick={previousSlide}>
         <span className="arrow">&#8592;</span>
-      </button>
+      </button> */}
       <div className="slider-container">
         {images.map((image, index) => {
-          let position = index - currentSlide;
-          if (position < 0) {
-            position += images.length;
-          }
-          if (position > images.length / 2) {
-            position -= images.length;
-          }
-          const style = {
-            transform: `translateX(${position * 100}%) scale(${position === 0 ? 1.2 : 0.8})`,
-            opacity: position === 0 ? 1 : 0.5,
-          };
           return (
             <img
               key={index}
-              className={`slide${position === 0 ? ' active' : ''}`}
-              src={image}
+              className={`slide${image.active ? ' active' : ''} cursor-pointer`}
+              src={image.image}
               alt={`Slide ${index}`}
+              onClick={() => handleImageClick(index)}
             />
           );
         })}
       </div>
-      <button className="next-button" onClick={nextSlide}>
+      {/* <button className="next-button" onClick={nextSlide}>
         <span className="arrow">&#8594;</span>
-      </button>
+      </button> */}
     </div>
   );
 }

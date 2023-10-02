d7d88b37a5683358d73c1fede3be389cdfa66325
Re-Creators <richardoehbet221@gmail.com>
2023-10-02 15:19:52 +0700

    update about us

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -4,25 +4,12 @@ import NewCarousel from "./NewCarousel";
 
 const About = () => {
   return (
-    <section className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-10 px-10 md:mx-0 items-center" id="about">
-      <div className="md:pl-36 md:pr-12 mt-10">
-        <div className="font-bold text-3xl md:text-4xl lg:text-4xl xl:text-4xl">ABOUT US</div>
-        <div className="mt-6">
-          <span className="font-bold text-base md:text-lg lg:text-xl xl:text-1xl mr-1">
-            PT. Satria Nusa Enjinering (Satria Nusa) 
-          </span>
-          <span className="text-base md:text-lg lg:text-xl xl:text-1xl">
-            merupakan perusahaan teknik yang didirikan dengan pengalaman lebih dari 30 tahun di sektor manufakturing dan pertambangan. Rekam jejak Satria Nusa dibuktikan dengan kepercayaan dari Teco, Ebara, Torishima dan Tsurumi untuk membantu pelanggan mereka di seluruh Indonesia untuk penggunaan yang tepat mutu dan tepat biaya.
-          </span>
-        </div>
-
-
-        <AboutLearnMore />
-      </div>
-      
-      <div className="w-full flex items-center justify-center relative mb-4">
+    <section
+      className="flex justify-center mt-10 px-10 md:mx-0 items-center"
+      id="about"
+    >
+      <div className="w-full lg:w-1/2 items-center justify-center relative mb-4">
         <NewCarousel />
-
       </div>
     </section>
   );

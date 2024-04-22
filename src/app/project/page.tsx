3253ef6ca0113d75b870932b6e553240d465e127
Farhan <faarhan7@gmail.com>
2024-04-22 09:40:14 +0700

    machine done

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -36,116 +36,116 @@ const page = () => {
             <p className="mt-5 text-base">
               {Translator.t("recent-client-desc", lang)}
             </p>
-            <div className="container">
-              <div className="grid w-full grid-cols-1 gap-5 mt-10 md:grid-cols-2 lg:grid-cols-3 ">
-                <div className="relative">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full "
-                    src="/img/project/recent_project/palm_oil.png"
-                    alt=""
-                  />
-                  <p className="absolute mr-2 text-3xl font-medium text-left text-white top-5 left-5">
-                    {Translator.t("palm-oil", lang)}
-                  </p>
-                  <a
-                    href="#"
-                    className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-5 bottom-5 hover:bg-[#0A3A6E]"
-                  >
-                    {Translator.t("view-more", lang)}
-                  </a>
-                </div>
-                <div className="relative">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full "
-                    src="/img/project/recent_project/fnb.png"
-                    alt=""
-                  />
-                  <p className="absolute text-3xl font-medium text-left text-white top-5 left-5">
-                    {Translator.t("fnb", lang)}
-                  </p>
-                  <a
-                    href="#"
-                    className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-5 bottom-5 hover:bg-[#0A3A6E]"
-                  >
-                    {Translator.t("view-more", lang)}
-                  </a>
-                </div>
-                <div className="relative">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full "
-                    src="/img/project/recent_project/hospital.png"
-                    alt=""
-                  />
-                  <p className="absolute text-3xl font-medium text-left text-white top-5 left-5">
-                    {Translator.t("hospital", lang)}
-                  </p>
-                  <a
-                    href="#"
-                    className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-5 bottom-5 hover:bg-[#0A3A6E]"
-                  >
-                    {Translator.t("view-more", lang)}
-                  </a>
-                </div>
-                <div className="relative">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full "
-                    src="/img/project/recent_project/power_plant.png"
-                    alt=""
-                  />
-                  <p className="absolute text-3xl font-medium text-left text-white top-5 left-5">
-                    {Translator.t("pp", lang)}
-                  </p>
-                  <a
-                    href="#"
-                    className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-5 bottom-5 hover:bg-[#0A3A6E]"
-                  >
-                    {Translator.t("view-more", lang)}
-                  </a>
-                </div>
-                <div className="relative">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full "
-                    src="/img/project/recent_project/shopping.png"
-                    alt=""
-                  />
-                  <p className="absolute text-3xl font-medium text-left text-white top-5 left-5">
-                    {Translator.t("sc", lang)}
-                  </p>
-                  <a
-                    href="#"
-                    className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-5 bottom-5 hover:bg-[#0A3A6E]"
-                  >
-                    {Translator.t("view-more", lang)}
-                  </a>
-                </div>
-                <div className="relative">
-                  <Image
-                    width={100}
-                    height={100}
-                    className="w-full "
-                    src="/img/project/recent_project/goods.png"
-                    alt=""
-                  />
-                  <p className="absolute text-3xl font-medium text-left text-white top-5 left-5">
-                    {Translator.t("gf", lang)}
-                  </p>
-                  <a
-                    href="#"
-                    className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-5 bottom-5 hover:bg-[#0A3A6E]"
-                  >
-                    {Translator.t("view-more", lang)}
-                  </a>
-                </div>
+          </div>
+          <div className="container mx-auto">
+            <div className="grid w-full grid-cols-1 gap-5 mt-10 md:grid-cols-2 lg:grid-cols-3 ">
+              <div className="relative">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full"
+                  src="/img/project/recent_project/palm_oil.png"
+                  alt=""
+                />
+                <p className="absolute mr-2 text-3xl font-medium text-left text-white top-10 left-10">
+                  {Translator.t("palm-oil", lang)}
+                </p>
+                <a
+                  href="#"
+                  className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-10 bottom-10 hover:bg-[#0A3A6E]"
+                >
+                  {Translator.t("view-more", lang)}
+                </a>
+              </div>
+              <div className="relative">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full "
+                  src="/img/project/recent_project/fnb.png"
+                  alt=""
+                />
+                <p className="absolute text-3xl font-medium text-left text-white top-10 left-10">
+                  {Translator.t("fnb", lang)}
+                </p>
+                <a
+                  href="#"
+                  className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-10 bottom-10 hover:bg-[#0A3A6E]"
+                >
+                  {Translator.t("view-more", lang)}
+                </a>
+              </div>
+              <div className="relative">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full "
+                  src="/img/project/recent_project/hospital.png"
+                  alt=""
+                />
+                <p className="absolute text-3xl font-medium text-left text-white top-10 left-10">
+                  {Translator.t("hospital", lang)}
+                </p>
+                <a
+                  href="#"
+                  className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-10 bottom-10 hover:bg-[#0A3A6E]"
+                >
+                  {Translator.t("view-more", lang)}
+                </a>
+              </div>
+              <div className="relative">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full "
+                  src="/img/project/recent_project/power_plant.png"
+                  alt=""
+                />
+                <p className="absolute text-3xl font-medium text-left text-white top-10 left-10">
+                  {Translator.t("pp", lang)}
+                </p>
+                <a
+                  href="#"
+                  className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-10 bottom-10 hover:bg-[#0A3A6E]"
+                >
+                  {Translator.t("view-more", lang)}
+                </a>
+              </div>
+              <div className="relative">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full "
+                  src="/img/project/recent_project/shopping.png"
+                  alt=""
+                />
+                <p className="absolute text-3xl font-medium text-left text-white top-10 left-10">
+                  {Translator.t("sc", lang)}
+                </p>
+                <a
+                  href="#"
+                  className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-10 bottom-10 hover:bg-[#0A3A6E]"
+                >
+                  {Translator.t("view-more", lang)}
+                </a>
+              </div>
+              <div className="relative">
+                <Image
+                  width={100}
+                  height={100}
+                  className="w-full "
+                  src="/img/project/recent_project/goods.png"
+                  alt=""
+                />
+                <p className="absolute text-3xl font-medium text-left text-white top-5 left-5">
+                  {Translator.t("gf", lang)}
+                </p>
+                <a
+                  href="#"
+                  className="text-base px-4 py-2 bg-[#0E4F94] text-white rounded-full absolute left-10 bottom-10 hover:bg-[#0A3A6E]"
+                >
+                  {Translator.t("view-more", lang)}
+                </a>
               </div>
             </div>
           </div>

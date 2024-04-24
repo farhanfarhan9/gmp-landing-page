5447af3e23343a8091dfee564ac15d6f675f6470
cecep31 <cecepjanuardi31@gmail.com>
2024-04-24 10:43:00 +0700

    about us translate

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,11 +1,15 @@
+"use client";
 import Advantages from "@/components/aboutus/Advantages";
 import CompanyJourney from "@/components/aboutus/CompanyJourney";
 import TeamProfile from "@/components/aboutus/TeamProfile";
-import Expand from "@/components/home/Expand";
+import Translator from "@/utils/Translator";
 import Image from "next/image";
+import { useSearchParams } from "next/navigation";
 import React from "react";
 
 function AboutUs() {
+  const searchParams = useSearchParams();
+  const lang = searchParams.get("lang") || undefined;
   return (
     <div>
       <div className="relative">
@@ -21,8 +25,9 @@ function AboutUs() {
       </div> */}
         <div className="absolute top-0 left-0 bottom-0 right-0 flex items-center">
           <div className="w-full mx-auto max-w-7xl text-5xl font-bold text-white">
-            Stepping into the Future <br />
-            from Experience to Innovation
+            <div className="max-w-2xl">
+              {Translator.t("about-us", lang)}
+            </div>
           </div>
         </div>
       </div>
@@ -32,35 +37,16 @@ function AboutUs() {
         height={800}
         alt=""
       />
-      <div className="mx-auto max-w-7xl mt-10 mb-64">
-        <div className="text-5xl font-bold text-[#0E4F94] text-center">
-        Stepping into the Future from <br />Experience to Innovation
+      <div className="mx-auto max-w-7xl mt-16 mb-64">
+        <div className="text-5xl max-w-3xl mx-auto font-bold text-[#0E4F94] text-center">
+          {Translator.t("about-us-title", lang)}
         </div>
         <div className="mt-7 text-base">
           <div>
-            With the success of building SNE into a National Engineering
-            Company. The company is committed to continuously improve internally
-            by meeting various occupational safety and quality management
-            compliance requirements. With this spirit, SNE already has
-            Occupational Safety and Health Management System (SMK3)
-            certification. Internal improvement is also followed by a
-            digitalization program since 2023 aimed at the implementation of
-            Good Corporate Governance. This stable, accountable & transparent
-            Internal System is proven to make SNE an Innovative Company. SNE
-            team always carry out work with a character full of responsibility
-            and sense of ownership.
+            {Translator.t("about-us-desc-1", lang)}
           </div>
           <div className="mt-7">
-            This character gives rise to various solutions for clients. As
-            evidence, SNE now has 1 patent for aeration systems. SNE is also
-            registering a patent for Dissolved Air Floatation (DAF) system. SNE
-            allocates a portion of our profits to ensure employee welfare and
-            Research & Development (R&D). As a proof of our seriousness to build
-            a sustainable and reliable Company. SNE built Knowledge Management
-            System (KMS) & Training Center aimed at quality & safety management
-            for all our works. SNE is committed to be a growing company by
-            continuously learning to provide the best solutions for our
-            customers.
+            {Translator.t("about-us-desc-2", lang)}
           </div>
         </div>
       </div>

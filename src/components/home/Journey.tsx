565129ce777824d229c6db90f13a89a867a4e41b
cecep31 <cecepjanuardi31@gmail.com>
2024-04-24 11:16:33 +0700

    advantages

--- src/components/home/Journey.tsx
+++ src/components/home/Journey.tsx
@@ -2,36 +2,38 @@
 import Impact from "./Impact";
 import About from "./About";
 import Image from "next/image";
+import Translator from "@/utils/Translator";
+import { useSearchParams } from "next/navigation";
+import Link from "next/link";
 
 function Journey() {
+  const searchParams = useSearchParams();
+  const lang = searchParams.get("lang") || undefined;
   return (
     <div className="relative bg-[#EDF2F5]">
-      <Image className="absolute top-28 opacity-10" src="/img/home/journeybg.png" alt="" width={400} height={300} />
+      <Image
+        className="absolute top-28 opacity-10"
+        src="/img/home/journeybg.png"
+        alt=""
+        width={400}
+        height={300}
+      />
       <Impact />
       <div className="flex pt-60 pb-32 relative mx-auto max-w-7xl">
         <div className="text-base">
-          <div className="text-[#0E4F94] text-5xl font-bold">
-            A Journey to <br /> Grow Together
+          <div className="text-[#0E4F94] text-5xl font-bold max-w-md">
+            {Translator.t("journey-title", lang)}
           </div>
-          <p className="mt-7">
-            Starting with experience in the electro-mechanical field since the
-            1990s and civil construction in 2011, PT Satria Nusa Enjinering
-            (SNE) was established with the aim of providing reliable water and
-            wastewater management solutions for Indonesia&apos;s manufacturing
-            industry
-          </p>
-          <p className="mt-7">
-            SNE was officially established in 2020 and continues to grow rapidly
-            with various successes throughout Indonesia. SNE has a workshop
-            facility in Deli Serdang equipped with specialized equipment for the
-            production of wastewater treatment machinery. SNE also has
-            laboratories in Medan, Jakarta & Surabaya that support our engineers
-            to collect and analyze data continuously for quality and
-            cost-effective system design.
-          </p>
-          <button className="text-white bg-[#0E4F94] py-2 px-4 mt-8 rounded-3xl">
-            Learn More
-          </button>
+          <div className="mt-7">{Translator.t("journey-desc-1", lang)}</div>
+          <div className="mt-7">{Translator.t("journey-desc-2", lang)}</div>
+          <div className="mt-10">
+            <Link
+              href={Translator.gotoUrl("/about-us", lang)}
+              className="text-white bg-[#0E4F94] py-2 px-4 rounded-3xl hover:bg-blue-700"
+            >
+              {Translator.t("learn-more", lang)}
+            </Link>
+          </div>
         </div>
         <div className="w-[500px]">
           <About />

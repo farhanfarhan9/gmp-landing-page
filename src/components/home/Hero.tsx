6f40a95c6c0bae8bee482076ce838b081fe9a7f2
cecep31 <cecepjanuardi31@gmail.com>
2024-04-24 13:50:22 +0700

    hero adjustment

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
     <section className="relative">
       <Image src={"/img/home/hero.png"} alt="" width={2000} height={400} />
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-50"></div>
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl text-center font-bold leading-snug">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white text-6xl -mt-10 text-center font-bold leading-snug">
         <div className="w-[800px]">
           {Translator.t("hero-1", lang)} <br />{" "}
           <div>{Translator.t("hero-2", lang)}</div>

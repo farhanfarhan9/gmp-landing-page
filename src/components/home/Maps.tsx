f5fcc4ad3dee47e7aa28d35dbbdee8b2e367aafb
Farhan <faarhan7@gmail.com>
2024-06-18 09:49:14 +0700

    google maps

--- src/components/home/Maps.tsx
+++ src/components/home/Maps.tsx
@@ -1,47 +1,15 @@
-"use client"
-import React, { useEffect } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
-
+"use client";
 const Maps = () => {
-    const mapRef = React.useRef<HTMLDivElement>(null);
-
-    const fetchMap = async () => {
-        // Prepare google maps API key
-        const loader = new Loader({
-            apiKey: process.env.NEXT_PUBLIC_API_KEY as string,
-            version: "weekly",
-        });
-
-        const { Map } = await loader.importLibrary("maps");
-        const {Marker} = await loader.importLibrary("marker")
-
-        // Company's position
-        const position = {
-            lat: 3.5965593888513068,
-            lng: 98.62950858023319,
-        };
-
-        // Map settings
-        const mapOptions: google.maps.MapOptions = {
-            center: position,
-            zoom: 17,
-            mapId: "GMP-Map",
-        };
-
-        const newMap = new Map(mapRef.current as HTMLDivElement, mapOptions);
-
-        // Map marker
-        const marker = new Marker({
-            map: newMap,
-            position: position
-        })
-    };
-
-    useEffect(()=>{
-        fetchMap()
-    },[])
-
-    return <div ref={mapRef} className="mx-auto h-96 md:max-w-7xl"/>;
+  // return <div ref={mapRef} className="mx-auto h-96 md:max-w-7xl"/>;
+  return (
+    <div className="mx-auto md:max-w-7xl">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3981.960899880017!2d98.6269765758131!3d3.596436250238584!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30312fd1b680511f%3A0xeaba4edd1b9cbd32!2sPT.%20Global%20Mitra%20Prima!5e0!3m2!1sen!2sid!4v1718678397440!5m2!1sen!2sid"
+        className="mx-auto h-96 md:w-full"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
 };
 
 export default Maps;

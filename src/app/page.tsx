f5cdb0d5f3b28fee6f72213996423974fe2696db
Farhan <faarhan7@gmail.com>
2024-07-01 11:07:36 +0700

    delete unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,11 @@
 import Hero from "@/components/home/Hero";
 import ProjectMap from "@/components/home/ProjectMap";
-import CompanyProfile from "@/components/home/CompanyProfile";
 import ProjectServices from "@/components/home/ProjectServices";
-import Principle from "@/components/home/Principle";
 import Outlets from "@/components/home/Outlets";
 import SalesProductivity from "@/components/home/SalesProductivity";
 import InfrastructureWarehouse from "@/components/home/InfrastructureWarehouse";
 import InfrastructureIt from "@/components/home/InfrastructureIt";
 import WarehouseFacility from "@/components/home/WarehouseFacility";
-import OurParner from "@/components/home/OurParner";
 import TotalEmployee from "@/components/home/TotalEmployee";
 import AwardAchievement from "@/components/home/AwardAchievement";
 import Partners from "@/components/home/Partners";
@@ -25,7 +22,6 @@ export default function HomePage() {
       <OurService  />
       <ProjectMap />
       <ProjectServices />
-      {/* <Principle /> */}
       <Outlets />
       <SalesProductivity />
       <InfrastructureWarehouse />
